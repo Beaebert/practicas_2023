603b555237b0aab6ba41d8b90caccc900bb0f440
EbertBeatriz <ebert.beatriz.be@gmail.com>
2023-06-04 19:25:34 -0300

    tema4

--- ejercicios_tema4/src/Main.java
+++ ejercicios_tema4/src/Main.java
@@ -1,25 +1,35 @@
 
 public class Main {
     public static void main(String[] args) {
-        var numeroIf = 2
+        int numeroIf = 0;
 
         if (numeroIf > 0) { System.out.println("Positivo");
         } else if (numeroIf < 0) {System.out.print("Negativo");
-        } else (numeroIf = 0) {
+        } else numeroIf = 0; {
             System.out.print("Cero");
         }
-        int i = 0;
-        while (i < 3) {
-            System.out.println(i);
-            i++;
+        int numeroWhile = 0;
+        while (numeroWhile < 3) {
+            System.out.println(numeroWhile);
+            numeroWhile++;
         }
 
-        do {
+        do {System.out.println(numeroWhile);
+            numeroWhile++;
+        } while (numeroWhile == 0);
 
-        } while () {
+        for (int numeroFor = 0; numeroFor <= 3; numeroFor++) {
+            System.out.println(numeroFor);
         }
 
-        switch () {
+        String Estacion = "Primavera";
+
+        switch (Estacion) {
+            case "Verano" -> System.out.println("Estamos en Verano");
+            case "Otoño" -> System.out.println("Estamos en Otoño");
+            case "Invierno" -> System.out.println("Estamos en Invierno");
+            case "Primavera" -> System.out.println("Estamos en Primavera");
+            default -> System.out.println("No se mencionó una Estación");
         }
     }
 }
