3b8ce9922cdeca4b030916122164afd404f6c3b9
EbertBeatriz <ebert.beatriz.be@gmail.com>
2023-06-04 19:21:47 -0300

    ejercicios de tema 4 terminados

---
+++ ejercicios_tema4/src/Main.java
@@ -0,0 +1,25 @@
+
+public class Main {
+    public static void main(String[] args) {
+        var numeroIf = 2
+
+        if (numeroIf > 0) { System.out.println("Positivo");
+        } else if (numeroIf < 0) {System.out.print("Negativo");
+        } else (numeroIf = 0) {
+            System.out.print("Cero");
+        }
+        int i = 0;
+        while (i < 3) {
+            System.out.println(i);
+            i++;
+        }
+
+        do {
+
+        } while () {
+        }
+
+        switch () {
+        }
+    }
+}
